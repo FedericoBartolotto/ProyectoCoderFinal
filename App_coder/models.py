24818ac13a3b838a7a998b33ee779e13ad7419ed
Federico Bartolotto <federicobartolotto@gmail.com>
2022-08-02 21:43:15 -0300

    inicio del proyecto

--- App_coder/models.py
+++ App_coder/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.shortcuts import render
 
 # Create your models here.
+
+class Curso(models.Model):
+
+    nombre = models.CharField(max_length=50)
+    camada = models.IntegerField()
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    documento = models.IntegerField()
+    fecha = models.DateField()
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.EmailField()
+    profesion = models.CharField(max_length=50)
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=50)
+    fechaDeEntrega = models.DateField()
+    entregado = models.BooleanField()
--- App_coder/views.py
+++ App_coder/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from App_coder.models import inicio, Estudiante, Profesor, Curso, Entregable
 
 # Create your views here.
+
+def inicio(self):
+    return HttpResponse("Vista inicio")
+    
+def estudiante(self):
+    return HttpResponse("Vista estudiante")
+
+def profesores(self):
+    return HttpResponse("Vista profesores")
+
+def curso(self):
+    return HttpResponse("Vista curso")
+
+def entregable(self):
+    return HttpResponse("Vista entregable")
